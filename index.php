ff0fd798e195818f228a0bc893b15f0ce1ffcb7d
Pindakees <162755391+Pindakeess@users.noreply.github.com>
2025-08-26 10:49:43 +0200

    pull + update footer
    Feat | Files gepulled en de footer weer toegevoegd

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styling/global.css">
+    <link rel="stylesheet" href="/styling/home.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php include("assets/nav/navbar.html"); ?>
+
+        <div class="hero-container">
+        <div class="hero-content-wrapper">
+            <h1>Lorem, ipsum dolor sit amet</h1>
+            <p>yoyoyoooo</p>
+            <div class="hero-button-container">
+                <a href="">awesomesauce</a>
+                <a id="h-btn-2" href="">meh</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
