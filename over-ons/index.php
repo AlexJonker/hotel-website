266ef7dca153d2f5b9ef51cdb3d01f75ba45bdae
Alex <alex.jonker2008@gmail.com>
2025-08-26 11:21:00 +0200

    split in multiple files

---
+++ over-ons/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/styling/global.css">
+  <link rel="stylesheet" href="/styling/over-ons.css">
+  <title>Over Ons - Hotel De Zonne Vallei.</title>
+  <script src="/over-ons/script.js"></script>
+</head>
+
+<body>
+  <div class="cont">
+    <h1>Wie zij wij?</h1>
+    <div class="box">
+
+      <div class="box2">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel adipisci nemo quibusdam voluptates expedita,
+          non nobis eveniet est, velit facilis itaque consequatur praesentium minima, labore accusamus libero nesciunt
+          error. Corrupti!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel adipisci nemo quibusdam voluptates expedita,
+          non nobis eveniet est, velit facilis itaque consequatur praesentium minima, labore accusamus libero nesciunt
+          error. Corrupti!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel adipisci nemo quibusdam voluptates expedita,
+          non nobis eveniet est, velit facilis itaque consequatur praesentium minima, labore accusamus libero nesciunt
+          error. Corrupti!</p>
+        <a href="http://127.0.0.1:5500/over-ons/index.html"><img
+            src="https://img.icons8.com/?size=100&id=18914&format=png&color=000000" width="50" height="50"
+            alt="alternatetext"><br> </a>
+      </div>
+      <div class="box3">
+
+        <div class="w3-content w3-section" style="max-width:500px">
+          <img class="mySlides"
+            src="https://fastly.picsum.photos/id/142/4272/2848.jpg?hmac=z8IS_an6FQ8ijJOBd-wSVg1JTZbeIDG4TbjHwLQbs0I"
+            style="width:105%" alt="alternatetext">
+          <img class="mySlides" src="https://picsum.photos/id/4/5000/3333" style="width:105%">
+          <img class="mySlides"
+            src="https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA"
+            style="width:105%">
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
