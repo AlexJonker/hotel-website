cd17b1ae70b4282a86a1e661b486fc7cf7ba8c16
Dominik <p.dominikkrakowski04@gmail.com>
2025-08-27 12:16:53 +0100

    FEAT: menu section op restaurant pagina

---
+++ hotel-website/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styling/global.css">
+    <link rel="stylesheet" href="/styling/home.css">
+    <title>Home - Hotel De Zonne Vallei</title>
+</head>
+<body>
+    <?php include("assets/html/navbar.html"); ?>
+
+    <div class="hero-container">
+        <div class="hero-content-wrapper">
+            <h1>Lorem, ipsum dolor sit amet</h1>
+            <p>yoyoyoooo</p>
+            <div class="hero-button-container">
+                <a href="">awesomesauce</a>
+                <a id="h-btn-2" href="">meh</a>
+            </div>
+        </div>
+    </div>
+
+    <section class="about-section">
+        <div class="about-content-wrapper">
+            <h2>Over Ons</h2>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi voluptates, quo consectetur doloremque voluptate dolor sint ipsum eius quisquam assumenda nemo facilis amet, dolores consequuntur beatae odio quidem quod molestiae.
+            </p>
+            <p>
+                jj hfhsdkf asdf f fjhffhf d d dkj a Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, rerum.
+            </p>
+            <a href="/over-ons" class="about-button">Meer over ons</a>
+        </div>
+    </section>
+
+
+    <?php include("assets/html/footer.html"); ?>
+</body>
+</html>
--- restaurant/index.php
+++ restaurant/index.php
@@ -15,7 +15,6 @@
 
 <?php include("../assets/html/navbar.html"); ?>
 
-
     <div class="hero-container">
         <div class="hero-content-wrapper">
             <h1>Welkom in ons Restaurant</h1>
@@ -30,7 +29,7 @@
     <section class="about-section">
         <div class="about-content-wrapper">
             <h2>Over het Restaurant</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius iure quasi aut architecto eum, mollitia et corrupti ipsa eligendi, perferendis quo ratione magnam pariatur saepe, dolorum natus eaque tempora voluptas?</p>
+            <p>Bij Hotel De Zonne Vallei zijn we trots op ons wereldberoemde restaurant, waar culinaire dromen werkelijkheid worden. Onze 2-duimen chef-kok, bekend om zijn creativiteit en vakmanschap, brengt zijn passie voor koken tot leven in elk gerecht. Met jarenlange ervaring in gerenommeerde keukens over de hele wereld, zorgt onze chef voor een onvergetelijke eetervaring die zowel traditionele als moderne smaken combineert.</p>
         </div>
     </section>
 
@@ -44,13 +43,44 @@
         </div>
     </section>
 
-    <section class="about-section2" id="menu">
+    <section class="about-section" id="menu">
         <div class="about-content-wrapper">
+            
             <h2>Menu</h2>
-            <ul style="list-style: none; padding: 0;">
-                <li>Voorgerecht: Tomatensoep</li>
-                <li>Hoofdgerecht: Gegrilde Zalm</li>
-                <li>Nagerecht: Chocolade Mousse</li>
+            <h3>Voorgerechten:</h3>
+            <ul>
+                <li>Geroosterde Bietensalade - Met geitenkaas, walnoten en een honing-balsamico dressing.</li>
+                <li>Carpaccio van Rundvlees - Met truffelmayonaise, rucola en Parmezaanse kaas.</li>
+                <li>Gerookte Zalm - Met dillecrème, kappertjes en een citroenpartje.</li>
+                <li>Ravioli met Spinazie en Ricotta - In een romige tomatensaus met basilicum.</li>
+                <li>Gegratineerde Champignons - Gevuld met knoflook en kruiden, geserveerd met knapperig brood.</li>
+            </ul>
+
+            <h3>Hoofdgerechten:</h3>
+            <ul>
+                <li>Gegrilde Zalmfilet - Met een citroen-dille saus, geserveerd met seizoensgroenten en aardappelpuree.</li>
+                <li>Ribeye Steak - Geserveerd met een pepersaus, gegrilde groenten en frietjes.</li>
+                <li>Kipfilet Supreme - Met een champignonroomsaus, gestoofde worteltjes en aardappelgratin.</li>
+                <li>Vegetarische Lasagne - Laagjes pasta, seizoensgroenten, ricotta en tomatensaus, geserveerd met een frisse salade.</li>
+                <li>Gebakken Zeebaars - Op een bedje van spinazie, geserveerd met rijst en een beurre blanc saus.</li>
+            </ul>
+
+            <h3>Nagerechten:</h3>
+            <ul>
+                <li>Tiramisu - Klassieke Italiaanse dessert met lagen van koffie-geweekte koekjes en mascarpone crème.</li>
+                <li>Crème Brûlée - Rijke vanille custard met een knapperige gekarameliseerde suikerlaag.</li>
+                <li>Chocolade Fondant - Warme chocoladetaart met een vloeibare kern, geserveerd met vanille-ijs.</li>
+                <li>Lemon Cheesecake - Romige cheesecake met een vleugje citroen en een graham cracker korst.</li>
+                <li>Vers Fruit met Sorbet - Een selectie van seizoensfruit, geserveerd met verfrissende sorbet.</li>
+            </ul>
+
+            <h3>Kinder Menu's:</h3>
+            <ul>
+                <li>Mini Burger - Met kaas en frietjes.</li>
+                <li>Kipnuggets - Geserveerd met appelmoes en een klein salade.</li>
+                <li>Spaghetti Bolognese - Met huisgemaakte tomatensaus.</li>
+                <li>Pannenkoeken - Met stroop en poedersuiker.</li>
+                <li>Fish and Chips - Gebakken vis met frietjes en een beetje ketchup.</li>
             </ul>
         </div>
     </section>
