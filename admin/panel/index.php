5b75345a1404d840132d1dfd1263ac645304a707
Dominik <p.dominikkrakowski04@gmail.com>
2025-08-27 14:30:51 +0100

    FEAT: Admin panel sidebar/aside

--- admin/panel/index.php
+++ admin/panel/index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/styling/global.css">
+    <link rel="stylesheet" href="/styling/home.css">
+    <link rel="stylesheet" href="/styling/panel.css">
     <?php include("../../assets/php/fontawesome.php"); ?>
 </head>
+
 <body>
-    <i class="fa-solid fa-house"></i>
+    <aside>
+        <nav>
+            <ul>
+                <li><a href=""><i class="fas fa-plus"></i> Toevoegen</a></li>
+                <li><a href=""><i class="fas fa-edit"></i> Editen</a></li>
+                <li><a href=""><i class="fas fa-key"></i> Wachtwoord wijzigen</a></li>
+            </ul>
+        </nav>
+        <a href="/" class="home-button" title="Home">
+            <i class="fas fa-home"></i>
+        </a>
+    </aside>
+
 </body>
+
 </html>
