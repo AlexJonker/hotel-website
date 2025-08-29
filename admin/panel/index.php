ac4716e3e2d3a9cbad6470362a61b6083fe21079
Alex <alex.jonker2008@gmail.com>
2025-08-29 15:52:01 +0200

    favicon and page name

--- admin/panel/index.php
+++ admin/panel/index.php
@@ -32,7 +32,8 @@ while ($row = mysqli_fetch_assoc($result)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Panel - Hotel De Zonne Vallei</title>
+    <link rel="icon" href="/assets/logos/favicon.ico">
     <link rel="stylesheet" href="/styling/global.css">
     <link rel="stylesheet" href="/styling/home.css">
     <link rel="stylesheet" href="/styling/panel.css">
