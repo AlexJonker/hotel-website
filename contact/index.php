56fc9cef4a421235f66534128ca5c6853e7769ca
Guled-01 <gmohamed@ictvanafmorgen.nl>
2025-09-01 11:23:00 +0200

    contact/overons

--- contact/index.php
+++ contact/index.php
@@ -1,13 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styling/global.css">
-    <link rel="stylesheet" href="/styling/contact.css">
-    <title>Contact - Hotel De Zonne Vallei</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/styling/global.css">
+  <link rel="stylesheet" href="/styling/over-ons.css">
+  <title>contact - Hotel De Zonne Vallei</title>
+  <script src="/over-ons/script.js"></script>
 </head>
+
 <body>
+  <?php include('../assets/html/navbar.html'); ?>
+  <div class="cont">
+    
+    <h1>Neem contact met ons</h1>
+    <div class="box">
+
+      <div class="box2">
+       <form>
+       <br>
+        
+        
+      
+     <label>Naam</label><br>
+      <input type="text" id="naam" name="naam" required ><br>
+       <br>
+         
+             <label>Email</label><br>
+      <input type="text" id="email" name="email" required ><br>
+    <label>Onderwerp</label> <br>
+      <input type="text" id="onderwerp" name="onderwerp" required ><br>
+
+          <label>message</label> <br>
+      <textarea id="email" name="email" rows="10" cols="80" required >Dit is een test Email</textarea>
+       </form>
+      </div>
+      <div class="box3">
+
+            <ul>
+                 <h3>contact info:</h3><br>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include('../assets/html/footer.html'); ?>
     
 </body>
 
