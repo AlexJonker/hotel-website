083bbaf630ecdc408bea3fa19a8701452137d736
Guled-01 <gmohamed@ictvanafmorgen.nl>
2025-09-02 11:14:52 +0200

    divfix v1

--- contact/index.php
+++ contact/index.php
@@ -35,41 +35,42 @@
     <h1>Neem contact met ons.</h1>
     <div class="box">
 
-      <div class="box2">
-        <form action='' method='post'>
+
+      <form action='' method='post'>
 
 
 
 
-          <h2 id='h'>Stel je vraag?</h2><br>
-          <input type="text" id="naam" name="naam" size="40" required placeholder="Naam" class="style">
+        <h2 id='h'>Stel je vraag?</h2><br>
+        <input type="text" id="naam" name="naam" size="40" required placeholder="Naam" class="style">
 
 
 
-          <input type="email" id="email" name="email" required placeholder="Email">
+        <input type="email" id="email" name="email" required placeholder="Email">
 
-          <textarea id="vraag" name="vraag" rows="4" cols="40" minlength="30" maxlength="800" required
-            placeholder="stel je vraag"></textarea>
-          <input type="submit" id="verzenden" name="verzenden" required>
-          <?php
+        <textarea id="vraag" name="vraag" rows="4" cols="40" minlength="30" maxlength="800" required
+          placeholder="stel je vraag"></textarea>
+        <input type="submit" id="verzenden" name="verzenden" required>
 
-          if (isset($_POST["verzenden"])) {
-            sender($_POST["naam"], $_POST["email"], $_POST["vraag"]);
+        <?php
 
-          } ?>
-      </div>
-      <div class="box3"> <!--section-->
+        if (isset($_POST["verzenden"])) {
+          sender($_POST["naam"], $_POST["email"], $_POST["vraag"]);
+
+        } ?>
+      </form>
+      <section>
+
+          <h2> <i class="fa-solid fa-info my-icon"></i>Contact info:</h2> <br>
 
         <ul>
-          <h2> <i class="fa-solid fa-info my-icon"></i>Contact info:</h2> <br> <!--wissel de namen met icoontjes-->
-
           <li><i class="fas fa-location-dot my-icon"></i> Straatnaam 85 1234 AB Alkmaar NL</a></li> <br>
           <!-- <li><i class="fas fa-phone my-icon"></i></a> ____________________</li> <br> -->
           <!-- <li> <i class="fas fa-envelope my-icon"></i> ____________________</a></li> -->
 
         </ul>
-        <!--section-->
-      </div>
+      </section>
+
     </div>
   </div>
 
