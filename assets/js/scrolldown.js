dc928f3efdd3996a413ea003e01ae23e481849bf
Pindakees <162755391+Pindakeess@users.noreply.github.com>
2025-09-09 09:46:43 +0200

    format scroldown knop

--- assets/js/scrolldown.js
+++ assets/js/scrolldown.js
@@ -1,7 +1,8 @@
 function scrollToNext(event) {
-    event.preventDefault();
-    const nextSection = document.querySelector(".hero-container").nextElementSibling;
-    if (nextSection) {
-        nextSection.scrollIntoView({ behavior: "smooth" });
-    }
+  event.preventDefault();
+  const nextSection =
+    document.querySelector(".hero-container").nextElementSibling;
+  if (nextSection) {
+    nextSection.scrollIntoView({ behavior: "smooth" });
+  }
 }
