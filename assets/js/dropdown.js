29ec6ad6b19a1f9237d9344b0bcda995ea8f8182
Pindakees <162755391+Pindakeess@users.noreply.github.com>
2025-09-09 09:47:18 +0200

    format dropdown

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,13 +1,13 @@
 function toggleDropdown(event) {
-    event.preventDefault();
-    document.getElementById("dropdown-content").classList.toggle("show");
+  event.preventDefault();
+  document.getElementById("dropdown-content").classList.toggle("show");
 }
 
 window.onclick = function (event) {
-    if (!event.target.closest('.dropdown')) {
-        let dropdowns = document.getElementsByClassName("dropdown-content");
-        for (let i = 0; i < dropdowns.length; i++) {
-            dropdowns[i].classList.remove('show');
-        }
+  if (!event.target.closest(".dropdown")) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].classList.remove("show");
     }
-}
+  }
+};
