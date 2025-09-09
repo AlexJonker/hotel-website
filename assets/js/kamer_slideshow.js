65106a98cff830d33c1e610fa3716c614316d0bb
Pindakees <162755391+Pindakeess@users.noreply.github.com>
2025-09-09 09:47:03 +0200

    format kamer foto's

--- assets/js/kamer_slideshow.js
+++ assets/js/kamer_slideshow.js
@@ -1,22 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const slides = document.querySelectorAll(".slide-container");
-    let current = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelectorAll(".slide-container");
+  let current = 0;
 
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            if (i === index) {
-                slide.style.display = '';
-            } else {
-                slide.style.display = 'none'; // Verstop de andere afbeeldingen
-            }
-        });
-        current = index;
-    }
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.style.display = "";
+      } else {
+        slide.style.display = "none";
+      }
+    });
+    current = index;
+  }
 
-    window.plusSlides = (n) => {
-        const next = (current + n + slides.length) % slides.length;
-        showSlide(next);
-    };
+  window.plusSlides = (n) => {
+    const next = (current + n + slides.length) % slides.length;
+    showSlide(next);
+  };
 
-    showSlide(current);
+  showSlide(current);
 });
